dfd0a08e603291ea00a13b382a2ca5a3c6cc38d0
ryokosuge <ryo.kosuge@gmail.com>
2023-05-26 15:01:18 +0900

    feat:	public cache control

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,19 @@
+const publicImages = ["favicon.ico", "next.svg", "vercel.svg"];
+
+const publicCacheHeader = {
+  key: "Cache-Control",
+  value: "public, max-age=2592000",
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  headers: () => [
+    ...publicImages.map((image) => ({
+      source: image,
+      headers: [publicCacheHeader],
+    })),
+  ],
 };
 
 module.exports = nextConfig;
