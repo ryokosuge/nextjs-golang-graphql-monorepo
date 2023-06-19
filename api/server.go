ef0c4ba667402895255a8b52cb6a42ee7c587664
ryokosuge <ryo.kosuge@gmail.com>
2023-06-19 21:29:20 +0900

    feat:	log

--- api/server.go
+++ api/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ryokosuge/nextjs-golang-graphql-monorepo/api/graph"
+	"github.com/ryokosuge/nextjs-golang-graphql-monorepo/api/graph/middleware"
 )
 
 const defaultPort = "8080"
@@ -21,7 +22,7 @@ func main() {
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", middleware.CheckAuthorization(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
