86d2b3c06eb5f1ba4cde187739a7c727f0bd1a64
ryokosuge <ryo.kosuge@gmail.com>
2023-06-21 11:54:09 +0900

    feat:	authしてuser info取得

--- api/graph/middleware/firebase.go
+++ api/graph/middleware/firebase.go
@@ -1,22 +1,68 @@
 package middleware
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+
+	"firebase.google.com/go/auth"
 )
 
-func CheckAuthorization(next http.Handler) http.Handler {
+type contextKey struct {
+	name string
+}
+
+var userCtxKey = &contextKey{"user"}
+
+type middleware struct {
+	auth *auth.Client
+}
+
+func NewAuthMiddleware(auth *auth.Client) *middleware {
+	return &middleware{
+		auth: auth,
+	}
+}
+
+func (m *middleware) CheckAuthorization(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorization := r.Header.Get("Authorization")
-		token, err := parseBearerToken(authorization)
+		sessionToken, err := parseBearerToken(authorization)
+
 		if err != nil {
+			log.Fatalln(err)
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte("Unauthorized"))
 			return
 		}
+
+		log.Println(sessionToken)
+
+		ctx := r.Context()
+		token, err := m.auth.VerifySessionCookie(ctx, sessionToken)
+		if err != nil {
+			log.Fatalln(err)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
+			return
+		}
+
 		log.Println(token)
+		user, err := m.auth.GetUser(ctx, token.Subject)
+		if err != nil {
+			log.Fatalln(err)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
+			return
+		}
+
+		log.Printf("%+v\n", user)
+		log.Printf("%+v\n", user.DisplayName)
+		log.Printf("%+v\n", user.Email)
+		ctx = context.WithValue(ctx, userCtxKey, user)
+		r = r.WithContext(ctx)
 		next.ServeHTTP(w, r)
 	})
 }
@@ -28,4 +74,17 @@ func parseBearerToken(header string) (string, error) {
 	}
 
 	return parts[1], nil
+}
+
+func GetUser(ctx context.Context) *auth.UserRecord {
+	val := ctx.Value(userCtxKey)
+	if val == nil {
+		return nil
+	}
+
+	user, ok := val.(*auth.UserRecord)
+	if !ok {
+		return nil
+	}
+	return user
 }
--- api/server.go
+++ api/server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	firebase "firebase.google.com/go"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ryokosuge/nextjs-golang-graphql-monorepo/api/graph"
@@ -14,16 +19,43 @@ import (
 const defaultPort = "8080"
 
 func main() {
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	app, err := firebase.NewApp(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	auth, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	firebaseAuth := middleware.NewAuthMiddleware(auth)
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", middleware.CheckAuthorization(srv))
+	http.Handle("/query", firebaseAuth.CheckAuthorization(srv))
+
+	go func() {
+		<-ctx.Done()
+
+		_, cancel := context.WithTimeout(context.Background(), time.Second*15)
+		defer cancel()
+	}()
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("server encountered some error", err)
+	}
 }
