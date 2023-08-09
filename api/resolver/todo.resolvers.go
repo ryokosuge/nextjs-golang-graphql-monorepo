727a575cd9531140d8c3ec67c2455663f51e8093
ryokosuge <ryo.kosuge@gmail.com>
2023-08-09 11:09:42 +0900

    feat:	WhereInput使ってQuery発行してみた

--- api/resolver/todo.resolvers.go
+++ api/resolver/todo.resolvers.go
@@ -21,6 +21,31 @@ func (r *mutationResolver) UpdateTodo(ctx context.Context, id int, input ent.Upd
 	return ent.FromContext(ctx).Todo.UpdateOneID(id).SetInput(input).Save(ctx)
 }
 
+// AllTodo is the resolver for the allTodo field.
+func (r *queryResolver) AllTodo(ctx context.Context, where *ent.TodoWhereInput, userWhere *ent.UserWhereInput) ([]*ent.Todo, error) {
+	q := r.client.Todo.Query().WithUser()
+
+	if userWhere != nil {
+		// userのfilterから始める
+		p, err := userWhere.P()
+		if err != nil {
+			return nil, err
+		}
+		uq := r.client.User.Query().Where(p)
+		q = uq.QueryTodos().WithUser()
+	}
+
+	if where != nil {
+		p, err := where.P()
+		if err != nil {
+			return nil, err
+		}
+		q = q.Where(p)
+	}
+
+	return q.All(ctx)
+}
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
